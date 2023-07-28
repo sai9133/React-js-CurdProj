4f24c86ba8f132f8dddf646e9153466bb10de1c6
sai9133 <gsaigsai19919@gmail.com>
2023-07-28 17:23:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import Homecrud from './component/Homecrud'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import CreateUser from './component/CreateUser'
+import User from './component/User'
+import Edituser from './component/Edituser'
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <Homecrud/>
+      <Routes>
+        <Route path="/create" element={<CreateUser/>}></Route>
+        <Route path="/user" element={<User/>}></Route>
+        <Route path="/edit/:index" element={<Edituser/>}></Route>
+      </Routes> 
+      
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Homecrud.jsx
@@ -0,0 +1,16 @@
+import style from "./home.module.css"
+import {Link} from "react-router-dom"
+import CreateUser from "./CreateUser"
+import User from "./User"
+const Homecrud=()=>
+{
+    return(
+        <section id={style.nav}>
+        <Link to="/create">CreateUser</Link>
+       <Link to="/user">user</Link>
+
+
+        </section>
+    )
+}
+export default Homecrud
